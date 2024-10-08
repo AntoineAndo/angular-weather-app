5a32385de38b3e62c5088017105aa10aaf518f68
Antoine Ando <antoine.ando@gmail.com>
2024-10-08 21:01:01 +0200

    Weather api implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 // import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/Header/Header.component';
+import { WeatherComponent } from './components/Weather/Weather.component';
+import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, WeatherComponent],
+  providers: [WeatherService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'angular-weather';
+  weatherData: any;
+
+  constructor(private weatherService: WeatherService) {}
+
+  ngOnInit() {
+    this.weatherService.weather$.subscribe((data) => {
+      this.weatherData = data;
+    });
+  }
 }
--- src/app/components/Header/Header.component.ts
+++ src/app/components/Header/Header.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { ThemeToggleComponent } from '../ThemeToggle/ThemeToggle.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, ThemeToggleComponent],
   templateUrl: './Header.component.html',
   styleUrl: './Header.component.scss',
 })
 export class HeaderComponent {
+  @Input() weatherData: any;
+
   title = 'angular-weather';
 }
